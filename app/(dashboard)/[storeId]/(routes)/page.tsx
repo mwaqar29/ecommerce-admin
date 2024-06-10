3b78a9789c7ea61db428408c1e04b797acc4a343
Waqar Ali <waqar.ali@wohlig.com>
2024-06-10 17:16:08 +0530

    navbar store switcher combobox complete

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,16 @@
-const Dashboard = () => {
-  return <div>Dashboard Page</div>
+import prisma from "@/lib/prisma"
+
+interface DashboardProps {
+  params: { storeId: string }
+}
+
+const Dashboard: React.FC<DashboardProps> = async ({ params }) => {
+  const store = await prisma.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  })
+  return <div>Active Store: {store?.name}</div>
 }
 
 export default Dashboard
---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+import { redirect } from "next/navigation"
+import prisma from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server"
+
+import { UserButton } from "@clerk/nextjs"
+import MainNav from "@/components/MainNav"
+import StoreSwitcher from "@/components/StoreSwitcher"
+
+export const Navbar = async () => {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect("/sign-in")
+  }
+
+  const stores = await prisma.store.findMany({
+    where: {
+      userId,
+    },
+  })
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
